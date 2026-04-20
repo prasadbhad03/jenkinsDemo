0e18e3922c9d72b86925c02c9187a573ce1f001b
prasadbhad03 <prasadbhad03@gmail.com>
2026-04-20 11:58:07 +0530

    Updated test.java
    Added Hello

--- test.java
+++ test.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is java program");
         System.out.println("Output for Jenkins test");
+        System.out.println("Hello");
        
         }
 }
