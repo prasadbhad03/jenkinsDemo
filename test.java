becdd2a4e0a924e2e7c459c691989904b82edeaf
prasadbhad03 <prasadbhad03@gmail.com>
2026-04-20 11:42:33 +0530

    Initial commit

---
+++ test.java
@@ -0,0 +1,8 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("This is java program");
+        System.out.println("Output for Jenkins test");
+       
+        }
+}
